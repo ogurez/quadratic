d6046e14af65accbdd366f2172f00d44c582ed5d
ogurez <ogurezslava@gmail.com>
2018-09-19 23:40:32 +0300

    delete function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+
+    var split=equation.split(' '),
+    a=+split[0],
+    b=+(split[3]+split[4]),
+    c=+(split[7]+split[8]);
+    
+    var Desc=Math.pow(b,2) - 4*a*c;
+    
+    var sol1=(-b +Math.sqrt(Desc))/(2*a);
+    var sol2=(-b -Math.sqrt(Desc))/(2*a);
+    var solutions=[];
+    
+    solutions.push(sol1);
+    solutions.push(sol2);
+    
+    solutions.sort();
+    
+    return solutions;
 }
